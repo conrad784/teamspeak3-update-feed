65839d09d2fea4b88eefb37702b993270c758912
Conrad Sachweh <conrad@csachweh.de>
2018-05-03 15:22:38 +0200

    clarify why you might not want to use the sync.sh

--- sync.sh
+++ sync.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-scp *.xml index.html medusa:/var/www/html/csachweh/ts3
+# only if you run the script manual and copy it to a remote ssh host
+scp *.{atom,rss} index.html medusa:/var/www/html/csachweh/ts3
